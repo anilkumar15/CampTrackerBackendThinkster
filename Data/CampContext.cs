0dbb593549cf9d9a175f1d6b200a722814627a25
DESKTOP-BI3L0RQ\TomBomb <thomasdes533@gmail.com>
2020-02-26 19:15:43 -0800

    initial commit

---
+++ Data/CampContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CampTracker.Models
+{
+    public class CampContext : DbContext
+    {
+        public CampContext (DbContextOptions<CampContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CampTracker.Models.Camp> Camp { get; set; }
+    }
+}
